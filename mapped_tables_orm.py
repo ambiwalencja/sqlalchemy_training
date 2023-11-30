8ba3813fd485a873ca30087421ff5db67354ccdc
ambiwalencja <helena.a.sokolowska@gmail.com>
2023-12-01 00:28:40 +0100

    changed schema in metadata from core to public

--- mapped_tables_orm.py
+++ mapped_tables_orm.py
@@ -2,7 +2,10 @@ from sqlalchemy import MetaData, Table
 from connection import engine, Base
 from sqlalchemy.orm import registry # rejestr przetrzymujący zmapowane tabele
 
-metadata = MetaData(schema='core') # tym razem engine nie potrzeba, core = dane tabeli będa według standardowych schematów postgresa
+metadata = MetaData(schema='public') # tutaj trzeba podać schema z bazy danych - u nas jest to public
+# ale np w przypadku bazy danych leancodu byłoby "core" bo oni mają nie postgres tylko coś innego
+# i wtedy parametr schema jest inaczej interpretowany, jako że 
+# dane tabeli mają być według standardowych schematów (cokolwiek to znaczy)
 
 plant_info = Table('plant_info', metadata, autoload_with=engine) # pierwsze dwa argumenty są pozycyjnem, nie potrzebują klucza
 # tutaj dokładnie to co w sqlowym podejściu - bierzemy informacje o tabeli i wrzucamy do obiektu klasy Table
--- put_plant_data_into_table.py
+++ put_plant_data_into_table.py
@@ -2,14 +2,21 @@ from mapped_tables_orm import PlantInfo
 from connection import Session
 from sqlalchemy import inspect
 
+# pobieramy informacje o tabeli
 mapper = inspect(PlantInfo)
-for column in mapper.attrs:
-    print(column.key)
+# for column in mapper.attrs:
+#     print(column.key, column.expression.type) # expression.type jest po to, żeby wyciągnąć typt wartości
+# no i tutaj dostaliśmy info:
+# id INTEGER
+# species VARCHAR(50)
+# edible BOOLEAN
+# height INTEGER
+# sunlight DOUBLE PRECISION
 
-# session = Session()
+session = Session()
 
-# # jak będziemy chcieli dodać rekord do takiej klasy z zaimportowanej tabeli, to nie dostaniemy 
-# # podpowiedzi, czym uzupełnić, tylko najpierw musielibyśmy sobie wyprintować, zeby to podejrzeć, jeśli nie znamy wyglądu tabeli
-# plant = PlantInfo()
+# jak będziemy chcieli dodać rekord do takiej klasy z zaimportowanej tabeli, to nie dostaniemy 
+# podpowiedzi, czym uzupełnić, tylko najpierw musielibyśmy sobie wyprintować, zeby to podejrzeć, jeśli nie znamy wyglądu tabeli
+plant = PlantInfo()
 
-# session.close()
+session.close()
